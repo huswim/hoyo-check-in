08ca2edb4f77ff7157c243757a25ad4d55feefe4
Hyeonuk Shin <haruswim@hanyang.ac.kr>
2025-03-21 11:19:18 +0900

    feat: Add installation method

--- README.md
+++ README.md
@@ -4,9 +4,31 @@
 
 ## 사용법
 
+### 주기적 실행
+
+[install.sh 다운로드](/install.sh)
+
+```bash
+sudo ./install.sh
+```
+
+### 일회성 실행
+
 1. .env 파일 작성
 
 ```
 LTOKEN_V2=""
 LTUID_V2=""
+```
+
+2. 첫 실행
+
+```bash
+docker run --name "hoyo-check-in" --env-file .env ghcr.io/huswim/hoyo-check-in:latest
+```
+
+3. 이후 실행
+
+```bash
+docker start hoyo-check-in
 ```
---
+++ install.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# 0.  LTOKEN_V2, LTUID_V2 입력
+read -p "LTOKEN_V2=" ${LTOKEN_V2}
+read -p "LTUID_V2=" ${LTUID_V2}
+
+# 1. 서비스 파일 생성
+cat <<EOF >/etc/systemd/system/hoyo-check-in.service
+[Unit]
+Description=Start hoyo-check-in Docker Container
+Requires=docker.service
+After=docker.service
+
+[Service]
+Type=oneshot
+ExecStart=/usr/bin/docker run --rm --name "hoyo-check-in" -e LTOKEN_V2=${LTOKEN_V2} -e LTUID_V2=${LTUID_V2} ghcr.io/huswim/hoyo-check-in:latest
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# 2. 타이머 파일 생성
+cat <<EOF >/etc/systemd/system/hoyo-check-in.timer
+[Unit]
+Description=Start hoyo-check-in Docker Container Daily
+
+[Timer]
+OnCalendar=*-*-* 06:00:00
+Persistent=true
+
+[Install]
+WantedBy=timers.target
+EOF
+
+# 3. systemd 재로드
+systemctl daemon-reload
+
+# 4. 타이머 활성화 및 시작
+systemctl enable hoyo-check-in.timer
+systemctl start hoyo-check-in.timer
+
+echo "Docker container start timer has been created and activated."
